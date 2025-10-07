7a5a8aa901c090c51e398ed0fe04c010caa0974e
sydur <eng.sydur@gmail.com>
2025-10-07 23:33:54 +0600

    first commit

---
+++ src/Pages/Apps.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Apps = () => {
+    return (
+        <div>
+            <h1>form apps</h1>
+        </div>
+    );
+};
+
+export default Apps;
---
+++ src/Pages/Root/Root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../../Componets/Header/Navbar';
+import Footer from '../../Componets/Footer/Footer';
+import { Outlet } from 'react-router';
+
+const Root = () => {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Root;
